05ffdafe8fc764f1e652a387792c02debe09972c
sw6794 <44162149+sw6794@users.noreply.github.com>
2021-09-18 13:43:57 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,25 +1,21 @@
 import discord
 from discord.ext import commands
 import os
+import random
 
-client = discord.Client()
 bot = commands.Bot(command_prefix = '$')
 
 @bot.event
 async def on_ready():
-  await client.change_presence(status=discord.Status.online)
-  await client.change_presence(activity=discord.Game(name="@help"))
   print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
 @bot.command()
 async def test(ctx, *args):
     num = len(args)
     if num == 2:
-        await ctx.send('**', args[1], '**\n`所持者(소지자) : ', args[0], '`')
-        
-        return
+        return await ctx.send('**', args[1], '**\n`所持者(소지자) : ', args[0], '`')
     elif num == 3:
-        await ctx.send(num)
+        return await ctx.send(num)
     else:
         return
 
