cb681e0765da448e41fa2b06c99e4cf65790c10d
sw6794 <44162149+sw6794@users.noreply.github.com>
2021-09-18 13:30:30 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 import os
 
 bot = commands.Bot(command_prefix = '$')
-
 @bot.event
 async def on_ready():
   await client.change_presence(status=discord.Status.online)
@@ -12,6 +11,14 @@ async def on_ready():
 
 @bot.command()
 async def test(ctx, *args):
-    await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
+    num = len(args)
+    if num == 2:
+        await ctx.send(num)
+    elif num == 3:
+        await ctx.send(num)
+    else:
+        return
+
+
 
 bot.run(os.environ['token'])
