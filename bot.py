107c561b50e4b2bf4fc978819cc8743f7ee8f274
sw6794 <44162149+sw6794@users.noreply.github.com>
2021-09-18 15:10:25 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -26,8 +26,13 @@ async def test(ctx, *args):
 
 @bot.command()
 async def end(ctx, msgID: int):
-     message = await ctx.fetch_message(msgID)
-     await ctx.send(message.content)
+    for channel in bot.get_all_channels():
+    try:
+        msg = await channel.get_message(msgID)
+    except NotFound:
+        continue
+    await ctx.send(msg.content)
+
 
 
 bot.run(os.environ['token'])
