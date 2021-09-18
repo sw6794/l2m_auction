6b86bd14ce1ed6d66d2489e71751d95de2c7401e
sw6794 <44162149+sw6794@users.noreply.github.com>
2021-09-18 14:38:27 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def on_ready():
 async def test(ctx, *args):
     num = len(args)
     if num == 2:
-        embed=discord.Embed(title="ID", description=msg.id)
+        embed=discord.Embed(title="ID", description=sent.id)
         embed.add_field(name="ITEM", value=args[1], inline=True)
         embed.add_field(name="所持者(소지자)", value=args[0], inline=True)
         embed.set_footer(text="参加者は以下に絵文字をクリックしてください。\n참여를 원하시면 이모티콘으로 반응해주세요.")
