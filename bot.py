af4b322b3b99f9c2fa2b60088b4c1987fcc67ba5
sw6794 <44162149+sw6794@users.noreply.github.com>
2021-09-18 19:00:20 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -46,8 +46,8 @@ async def end(ctx, link: str):
     if len(users) != 0:
         entry=len(users)
 
-        print(userids)
-        winner = random.choice(list(userids))
+        userlist = list(userids)
+        winner = random.choice(userlist)
 
         embed=msg.embeds[0]
         embed.set_footer(text="")
@@ -55,8 +55,8 @@ async def end(ctx, link: str):
         embed.add_field(name="当選者(당선자)", value=f"<@{winner}>", inline=False)
         await ctx.send(embed=msg.embeds[0])
 
-        embed2=discord.Embed(title="FINISHED")
-        await msg.edit(embed=embed2)
+        #embed2=discord.Embed(title="FINISHED")
+        #await msg.edit(embed=embed2)
 
 
     else:
