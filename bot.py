ce2908a3eef40793a376c388357128e1e10dfb79
sw6794 <44162149+sw6794@users.noreply.github.com>
2021-09-20 16:57:32 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -89,7 +89,7 @@ async def endt(ctx, link: str):
 
         userlist = list(userids)
         winner = random.choice(userlist)
-        member = bot.get_user(int(winner))
+        member = bot.get_member(int(winner))
         print(member)
 
         embed=msg.embeds[0]
