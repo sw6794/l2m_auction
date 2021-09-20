fdf5b428e778864802e8d252b975164c77e2f2c8
sw6794 <44162149+sw6794@users.noreply.github.com>
2021-09-20 17:43:03 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -19,9 +19,10 @@ async def on_ready():
 @bot.command()
 async def auction(ctx, *args):
     num = len(args)
-    if num == 2:
+    if num == 3:
         embed=discord.Embed()
-        embed.add_field(name="ITEM", value=args[1], inline=True)
+        embed.add_field(name="ITEM", value=args[2], inline=True)
+        embed.add_field(name="BOSS", value=args[1], inline=True)
         embed.add_field(name="所持者(소지자)", value=args[0], inline=True)
         embed.set_footer(text="参加者は以下に絵文字をクリックしてください。\n참여를 원하시면 이모티콘으로 반응해주세요.")
 
